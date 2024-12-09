8b39acdc5b2a6f68092035e7152b5d0394960ec8
NikolaiQuasarka <nikolaiquasarka@gmail.com>
2024-12-09 20:34:08 +0300

    Перенос списка сообщений в отдельный компонент

--- src/pages/Index/Index.jsx
+++ src/pages/Index/Index.jsx
@@ -3,6 +3,7 @@ import { getMessages, sendMessage, messagesRef } from "../../apis/messages"
 import { Form, useLoaderData, useNavigation } from "react-router"
 import { useEffect, useRef, useState } from "react"
 import { onChildAdded } from "firebase/database"
+import MessageList from "./components/MessageList"
 
 export async function loader() {
 	const data = await getMessages()
@@ -19,22 +20,8 @@ export async function action({ request }) {
 export default function Index() {
 	const loaderData = useLoaderData()
 	const [messages, setMessages] = useState(loaderData)
-	const messagesListElement = useRef(null)
-	const bottom = useRef(null)
-	const isScrolledToBottom = useRef(true)
 	const navigation = useNavigation()
 	const formRef = useRef()
-
-	const messagesElements = function () {
-		return messages.map((message) => {
-			return (
-				<article className="message" key={message.key}>
-					<div className="sender">{message.key}</div>
-					<div className="text">{message.content}</div>
-				</article>
-			)
-		})
-	}
 
 	useEffect(() => {
 		const unsubscribe = onChildAdded(messagesRef, (data) => {
@@ -44,38 +31,13 @@ export default function Index() {
 		})
 		return () => unsubscribe()
 	}, [])
-
-	useEffect(() => {
-		const handleScroll = () => {
-			const element = messagesListElement.current
-			const isAtBottom =
-				element.scrollHeight - element.scrollTop ===
-				element.clientHeight
-			isScrolledToBottom.current = isAtBottom
-		}
-		const element = messagesListElement.current
-		element.addEventListener("scroll", handleScroll)
-		return () => {
-			element.removeEventListener("scroll", handleScroll)
-		}
-	}, [])
-	useEffect(() => {
-		const bottomElement = bottom.current
-		if (isScrolledToBottom.current) {
-			bottomElement.scrollIntoView({ behavior: "smooth" })
-		}
-	}, [messages])
-
 	useEffect(() => {
 		if (navigation.state === "idle") formRef.current.reset()
 	}, [navigation.state])
 	return (
 		<div id="Index">
 			<main>
-				<section className="message-list" ref={messagesListElement}>
-					{messagesElements()}
-					<div className="bottom" ref={bottom}></div>
-				</section>
+				<MessageList messages={messages} />
 				<Form method="POST" replace ref={formRef}>
 					<div className="text-border">
 						<textarea name="content" />
---
+++ src/pages/Index/components/MessageList.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useRef } from "react"
+
+export default function MessageList({ messages }) {
+	const messagesListElement = useRef(null)
+	const bottom = useRef(null)
+	const isScrolledToBottom = useRef(true)
+	const messagesElements = function () {
+		return messages.map((message) => {
+			return (
+				<article className="message" key={message.key}>
+					<div className="sender">{message.key}</div>
+					<div className="text">{message.content}</div>
+				</article>
+			)
+		})
+	}
+	useEffect(() => {
+		const handleScroll = () => {
+			const element = messagesListElement.current
+			const isAtBottom =
+				element.scrollHeight - element.scrollTop ===
+				element.clientHeight
+			isScrolledToBottom.current = isAtBottom
+		}
+		const element = messagesListElement.current
+		element.addEventListener("scroll", handleScroll)
+		return () => {
+			element.removeEventListener("scroll", handleScroll)
+		}
+	}, [])
+	useEffect(() => {
+		const bottomElement = bottom.current
+		if (isScrolledToBottom.current) {
+			bottomElement.scrollIntoView({ behavior: "smooth" })
+		}
+	}, [messages])
+	return (
+		<section className="message-list" ref={messagesListElement}>
+			{messagesElements()}
+			<div className="bottom" ref={bottom}></div>
+		</section>
+	)
+}
