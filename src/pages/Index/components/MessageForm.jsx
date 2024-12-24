6256fb290606384bfd57b0cccf9afe87b8373eb7
NikolaiQuasarka <nikolaiquasarka@gmail.com>
2024-12-25 00:38:23 +0300

    Доабавлена возможность отправик сообщения на Enter и невозможность отправки пустого сообщения

--- src/pages/Index/components/MessageForm.jsx
+++ src/pages/Index/components/MessageForm.jsx
@@ -1,18 +1,38 @@
 import { Form, useNavigation } from "react-router"
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 export default function MessageForm() {
 	const navigation = useNavigation()
 	const formRef = useRef()
+	const textareaRef = useRef()
+	const [formText, setFormText] = useState("")
+
 	useEffect(() => {
-		if (navigation.state === "idle") formRef.current.reset()
+		if (navigation.state === "idle") setFormText("")
 	}, [navigation.state])
 
+	function sendMessage(e) {
+		if (e.keyCode === 13) {
+			e.preventDefault()
+			formRef.current.requestSubmit()
+		}
+	}
 	return (
 		<Form method="POST" replace ref={formRef}>
 			<div className="text-border">
-				<textarea name="content" />
+				<textarea
+					ref={textareaRef}
+					name="content"
+					onKeyDown={sendMessage}
+					value={formText}
+					onInput={(e) => setFormText(e.target.value)}
+				/>
 			</div>
-			<input type="submit" value="Отправить" />
+			<input
+				type="submit"
+				value="Отправить"
+				onClick={() => textareaRef.current.focus()}
+				disabled={formText ? false : true}
+			/>
 		</Form>
 	)
 }
