c3b374d94da422799c9e78a29032ccb846139c4b
NikolaiQuasarka <nikolaiquasarka@gmail.com>
2024-12-25 00:37:05 +0300

    Добавлено перенаправление на login при выходе из аккаунта

--- src/pages/components/Header.jsx
+++ src/pages/components/Header.jsx
@@ -3,10 +3,13 @@ import { getUnAuthorized, isAuthorized } from "../../apis/auth"
 import { useEffect, useState } from "react"
 import { onAuthStateChanged } from "firebase/auth"
 import { auth } from "../../apis/firebseConfig"
+import { useNavigate } from "react-router"
 
 export default function Header() {
+	const navigate = useNavigate()
 	async function leaveAccountOnClickHandle() {
 		await getUnAuthorized()
+		navigate("/account/login")
 	}
 	const [isLeaveButtonVisible, setIsLeaveButtonVisible] = useState(false)
 	useEffect(() => {
